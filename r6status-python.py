37b7ff569c717802cf17f10ba8600760420c3aa3
upaver20 <upa.ver.2.0@gmail.com>
2017-12-03 07:50:26 +0900

    overhaul

---
+++ r6status-python.py
@@ -0,0 +1,54 @@
+import r6sapi
+import asyncio
+import json
+import sys
+
+@asyncio.coroutine
+
+def run():
+    config = json.load(open('config.json','r'))
+    file = open('player_data.json','w')
+
+    mail = config["e-mail address"]
+    pswd = config["password"]
+    players = config["players"]
+
+    try:
+        auth = r6sapi.Auth( mail, pswd )
+    except:
+        print("Email address and password do not match")
+        sys.exit(1)
+
+    players_data=[]
+
+    for player_id in players:
+
+        try:
+            player = yield from auth.get_player(player_id, r6sapi.Platforms.UPLAY)
+
+        except:
+            print (player_id + "is not found")
+            break
+
+        else:
+            yield from player.load_general()
+            yield from player.load_level()
+            rank_data = yield from player.get_rank(r6sapi.RankedRegions.ASIA,7)
+
+            player_data = {
+                "id" : player.name,
+                "level" : player.level,
+                "icon"  : player.icon_url,
+                "rank"  : rank_data.rank,
+                "kills" : player.kills,
+                "deaths" : player.deaths,
+                "K/D Ratio" : player.kills / player.deaths,
+                "wons" : player.matches_won,
+                "loses" : player.matches_lost,
+                "Win Ratio" : player.matches_won / player.matches_played
+            }
+        players_data.append(player_data)
+
+    json.dump(players_data,file,indent=4,sort_keys=True)
+
+asyncio.get_event_loop().run_until_complete(run())
