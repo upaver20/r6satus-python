dcab0a9b807b7ae12e405e02db89bdf0e244a936
upaver20 <upa.ver.2.0@gmail.com>
2017-12-03 18:28:00 +0900

    Add information for each game mode

--- r6status-python.py
+++ r6status-python.py
@@ -13,8 +13,9 @@ def run():
     pswd = config["password"]
     players = config["players"]
 
+    auth = r6sapi.Auth( mail, pswd )
+
     try:
-        auth = r6sapi.Auth( mail, pswd )
         yield from auth.connect()
     except:
         print("Email address and password do not match")
@@ -31,8 +32,9 @@ def run():
             print (player_id + " is not found")
             continue
 
-        yield from player.load_general()
-        yield from player.load_level()
+        yield from player.check_general()
+        yield from player.check_level()
+        yield from player.load_queues()
         rank_data = yield from player.get_rank(r6sapi.RankedRegions.ASIA,7)
 
         player_data = {
@@ -40,13 +42,30 @@ def run():
             "level" : player.level,
             "icon"  : player.icon_url,
             "rank"  : rank_data.rank,
-            "kills" : player.kills,
-            "deaths" : player.deaths,
-            "K/D Ratio" : player.kills / player.deaths,
-            "wons" : player.matches_won,
-            "loses" : player.matches_lost,
-            "W/L Ratio" : player.matches_won / player.matches_lost
+            "general" : {
+                "kills" : player.kills,
+                "deaths" : player.deaths,
+                "K/D Ratio" : round( player.kills / player.deaths,2 ) ,
+                "wons" : player.matches_won,
+                "loses" : player.matches_lost,
+                "played": player.matches_played,
+                "play time" : player.time_played,
+                "W/L Ratio" : round( player.matches_won / player.matches_lost, 2)
+            }
         }
+
+        for gamemode in [ player.casual, player.ranked ]:
+            player_data[ gamemode.name ] = {
+                "kills" : gamemode.kills,
+                "deaths" : gamemode.deaths,
+                "K/D Ratio" : round( gamemode.kills / gamemode.deaths, 2),
+                "wons" : gamemode.won,
+                "loses" : gamemode.lost,
+                "played": gamemode.played,
+                "play time" : gamemode.time_played,
+                "W/L Ratio" : round(player.matches_won / player.matches_lost, 2)
+            }
+
         players_data.append(player_data)
 
     json.dump(players_data,file,indent=4,sort_keys=True)
